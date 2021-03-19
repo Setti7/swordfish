148fe77142f210b7e745b221abe11fb5451d3f3f
André Niero Setti <ansetti7@gmail.com>
2021-03-19 16:14:50 -0300

    Remove unused storybook scenario

--- src/components/Hero/Hero.stories.tsx
+++ src/components/Hero/Hero.stories.tsx
@@ -1,23 +1,17 @@
-import React from "react"
-import { Story, Meta } from "@storybook/react/types-6-0"
-import { Hero, HeroProps } from "."
+import React from "react";
+import { Story, Meta } from "@storybook/react/types-6-0";
+import { Hero, HeroProps } from ".";
 
 export default {
   title: "Component/Hero",
   component: Hero,
   argTypes: {
-    title: { control: "string" }
-  }
-} as Meta
+    title: { control: "string" },
+  },
+} as Meta;
 
-const Template: Story<HeroProps> = (args) => <Hero {...args} />
+const Template: Story<HeroProps> = (args) => <Hero {...args} />;
 
 // Default scenario
-export const Default = Template.bind({})
-Default.args = {}
-
-// Title = "Hello World"
-export const TitleHelloWorld = Template.bind({})
-TitleHelloWorld.args = {
-  title: "Hello World"
-}
+export const Default = Template.bind({});
+Default.args = {};
